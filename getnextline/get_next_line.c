2fc02381a73698f655bf4fdff5c34dfae30bb300
semlox <74025428+semlox@users.noreply.github.com>
2021-12-09 20:39:44 +0100

    Add files via upload

---
+++ getnextline/get_next_line.c
@@ -0,0 +1,83 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lsemlali <lsemlali@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/12/09 19:57:20 by lsemlali          #+#    #+#             */
+/*   Updated: 2021/12/09 20:32:37 by lsemlali         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+#include <fcntl.h>
+#include <stdio.h>
+
+char	*ft_read_f(int fd, char *buff)
+{
+	int		i;
+	int		j;
+	char	*line;
+
+	line = NULL;
+	if (buff[0] != 0)
+		line = ft_strjoin(line, buff);
+	while (!ft_search(buff))
+	{
+		j = 0;
+		while (buff[j])
+			buff[j++] = 0;
+		i = read(fd, buff, BUFFER_SIZE);
+		if (i < 0 || (i == 0 && !line))
+			return (0);
+		line = ft_strjoin(line, buff);
+		if (i < BUFFER_SIZE && line)
+			return (line);
+	}
+	return (line);
+}
+
+char	*ft_join(int fd, char *buff)
+{
+	char	*line;
+	int		i;
+
+	if (ft_search(buff))
+	{
+		line = ft_substr(buff, 0, ft_strlen(buff));
+		if (!line)
+			return (NULL);
+		ft_cut(buff);
+	}
+	else
+	{
+		i = 1;
+		line = ft_read_f(fd, buff);
+		ft_cut(buff);
+	}
+	return (line);
+}
+
+char	*get_next_line(int fd)
+{
+	char		*line;
+	static char	buff[BUFFER_SIZE + 1];
+
+	line = NULL;
+	if (fd < 0 || read(fd, buff, 0) < 0 || BUFFER_SIZE <= 0)
+		return (0);
+	line = ft_join(fd, buff);
+	return (line);
+}
+
+/*int	main(void)
+{
+	int	fd;
+
+	fd = open("file.txt", O_RDONLY);
+	printf("%s", get_next_line(fd));
+	printf("%s", get_next_line(fd));
+	printf("%s", get_next_line(fd));
+	printf("%s", get_next_line(fd));
+}*/
